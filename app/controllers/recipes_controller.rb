3f65f5cd3d42f3eabd449bed626fb70475175096
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-11-15 13:35:31 +0100

    Add recipes controllers and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,26 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    # @recipes = Recipe.all
+    @recipes = current_user.recipes
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to user_recipes_path, notice: 'Recipe was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -14,5 +30,11 @@ class RecipesController < ApplicationController
     @recipe.save
     flash[:notice] = 'recipe was successfully deleted'
     redirect_to user_recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :public, :cooking_time, :preparation_time)
   end
 end
