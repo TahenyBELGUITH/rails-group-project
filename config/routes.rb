8043b4e829d2d61d68c2031cdefd9c3463948c09
Sevenpros <sethbisoso@gmail.com>
2022-11-16 10:42:09 +0100

    Add recipe link in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root "foods#index"
   resources :recipes, only: [:new, :create, :destroy, :index, :show]
   resources :foods, only: [:destroy, :index, :new, :create]
-  resources :public_recipes, only: [:index :show]
+  resources :public_recipes, only: [:index, :show]
   resources :general_shopping_list, only: [:index]
   resources :recipes, only: [:index] do
     resources :recipe_foods, only: [ :new, :create, :destroy, :index]
